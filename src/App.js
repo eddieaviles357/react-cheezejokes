35cda4f9a64c17a0535ffa5062028f6a2263d27b
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-27 19:43:35 -0700

    Refactored from class component to function component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import JokeList from "./JokeList";
 
 /** App component. Renders list of jokes. */
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <div className="App">
         <JokeList />
       </div>
     );
-  }
-}
+};
 
 export default App;
